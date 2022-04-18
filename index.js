1511290e2515230a1044876b5ffb8f12f32f04cf
CHARLEY CARRIERO <carriero.char@gmail.com>
2022-04-18 15:51:30 +0200

    attempt to fix port .env

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const router = require('./router');
 
 const app = express();
+const { PORT } = process.env;
 
 app
 	.use(morgan('dev'))
@@ -12,4 +13,6 @@ app
 	.use(router)
   .get('/', (_, res) => { res.status(200).send('Hello, stranger!'); })
   .get('*', (_, res) => { res.status(404).send('Sorry, not found 😞'); })
-	.listen(3005);
+  .listen(PORT, () => {
+    console.log(`🚀 server listening on port: ${PORT}`);
+  });
