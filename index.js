fc1bacdca590b5272294ec1b2d3f6ba07a6d19ab
CHARLEY CARRIERO <carriero.char@gmail.com>
2022-04-18 15:44:35 +0200

    added default response and 404

--- index.js
+++ index.js
@@ -10,4 +10,6 @@ app
 	.use(cors())
 	.use(express.json())
 	.use(router)
+  .get('/', (_, res) => { res.status(200).send('Hello, stranger!'); })
+  .get('*', (_, res) => { res.status(404).send('Sorry, not found 😞'); })
 	.listen(3005);
